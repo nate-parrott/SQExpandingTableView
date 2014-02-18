fe0fd3a5b286f1bda39963881af99c4f4a30f336
nate-parrott <nateparro2t@gmail.com>
2014-02-18 16:49:49 -0500

    Initial Commit

---
+++ SQBetaUI/SQAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SQAppDelegate.h
+//  SQBetaUI
+//
+//  Created by Nate Parrott on 2/18/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SQAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SQBetaUI/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SQBetaUI
+//
+//  Created by Nate Parrott on 2/18/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SQAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SQAppDelegate class]));
+    }
+}
